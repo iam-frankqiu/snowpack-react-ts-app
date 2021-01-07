deeb3f853fbfbb6df050289980fdc403d3927d03
FrankQiu <iam.frankqiu@gmail.com>
2021-01-07 11:23:18 +0800

    add react-app presets

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,31 @@ module.exports = {
     src: { url: '/dist' },
   },
   plugins: [
+    [
+      '@snowpack/plugin-babel',
+      {
+        transformOptions: {
+          presets: [
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ],
+          plugins: [
+            [
+              '@babel/plugin-proposal-decorators',
+              {
+                legacy: true,
+              },
+            ],
+            [
+              "@babel/plugin-proposal-class-properties",
+              {
+                "loose": true
+              }
+            ]
+          ],
+        },
+      },
+    ],
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
