fe8f8f6383fc066d62106b514ebb48aeae49f7d2
FrankQiu <iam.frankqiu@gmail.com>
2021-01-07 11:06:03 +0800

    fix: fix bug

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,35 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
+    [
+      '@snowpack/plugin-babel',
+      {
+        transformOptions: {
+          presets: [
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ],
+          plugins: [
+            [
+              '@babel/plugin-proposal-decorators',
+              {
+                legacy: true,
+              },
+            ],
+            [
+              "@babel/plugin-proposal-class-properties",
+              {
+                "loose": true
+              }
+            ]
+          ],
+        },
+      },
+    ],
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
